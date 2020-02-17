8b04a2da053051ea95daaa09c61004547fc09c99
Dojun Park <dojun.parkk@gmail.com>
2020-02-17 19:21:57 +0900

    update exercise

---
+++ example.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is a example for converting ipynb to py
+
+# In[4]:
+
+
+a = 10
+b = 20
+
+ls_sample = []
+for num1 in range(1, a+1):
+    for num2 in range(1, b+1):
+        ls_sample.append((num1, num2))
+
+display(ls_sample)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
